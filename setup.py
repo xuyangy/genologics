c25ee94a3a3359425720a64f7e696fae96c91ed0
vagrant <vagrant@precise64.(none)>
2013-03-04 12:50:51 +0000

    Add __init__.py to package and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.2'
+version = '0.2.1'
 
 setup(name='genologics',
       version=version,
       description="Python interface to the GenoLogics LIMS (Laboratory Information Management System) server via its REST API.",
       long_description="""A basic module for interacting with the GenoLogics LIMS server via its REST API.
                           The goal is to provide simple access to the most common entities and their attributes in a reasonably Pythonic fashion.""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+	"Development Status :: 4 - Beta",
+	"Environment :: Console",
+	"Intended Audience :: Developers",
+	"Intended Audience :: Healthcare Industry",
+	"Intended Audience :: Science/Research",
+	"License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+	"Operating System :: POSIX :: Linux",
+	"Programming Language :: Python",
+	"Topic :: Scientific/Engineering :: Medical Science Apps."
+	],
       keywords='genologics api rest',
       author='Per Kraulis',
       author_email='per.kraulis@scilifelab.se',
