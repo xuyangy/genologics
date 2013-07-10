369371353297427f9add2d0deef0fa6bf98b87dd
Johannes Alneberg <alneberg@kth.se>
2013-07-10 09:40:25 +0200

    attach caliper file adapted to post file function

--- examples/attach_caliper_file.py
+++ examples/attach_caliper_file.py
@@ -9,17 +9,30 @@ Johannes Alneberg, Science for Life Laboratory, Stockholm, Sweden.
 
 from pprint import pprint
 from genologics.lims import Lims
-from genologics.entities import Artifact
+from genologics.entities import Artifact, Glsstorage, File
+from shutil import copy
+from xml.etree import ElementTree
 
 from genologics.config import BASEURI, USERNAME, PASSWORD
+import os
 
 lims = Lims(BASEURI,USERNAME,PASSWORD)
 lims.check_version()
 
-artifact = Artifact(lims,id='2-26065')
+artifact = Artifact(lims,id='92-26375')
 
 print 'UDFs:'
 pprint(artifact.udf.items())
 
 print 'Files:'
 pprint(artifact.files)
+
+print 'Artifact uri:'
+pprint(artifact.uri)
+
+# Allocate location at the LIMS server
+original_location='/home/johannes/repos/genologics/test_data/2-26065.JPG'
+assert os.path.isfile(original_location)
+response = lims.post_file(artifact,original_location)
+
+print response
