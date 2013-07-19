59e94011d691fdbac1d6bcb7ef4f7ea9c1dfe248
Johannes Alneberg <johannes.alneberg@scilifelab.se>
2013-07-19 13:57:41 +0200

    HEllo world script for epp

---
+++ examples/epp_script.py
@@ -0,0 +1,12 @@
+"""Python interface to GenoLogics LIMS via its REST API.
+
+Usage example: EPP example script for Clarity LIMS.
+
+
+Johannes Alneberg, Science for Life Laboratory, Stockholm, Sweden.
+"""
+from optparse import OptionParser
+
+if __name__ == "__main__":
+    print "Hello World!"
+
