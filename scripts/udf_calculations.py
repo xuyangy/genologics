9695abd503a55056b85cababc80fd2c49563e3d7
Johannes Alneberg <johannes.alneberg@scilifelab.se>
2013-07-26 15:24:55 +0200

    op not operator

--- scripts/udf_calculations.py
+++ scripts/udf_calculations.py
@@ -35,7 +35,7 @@ def apply_calculations(lims,input_artifacts,udf1,op,udf2,result_udf):
             print artifact.udf.items()
 
         print 'Updating: Artifact id: {0}, result_udf: {1}, udf1: {2}, operator: {3}, udf2: {4}'.format(
-            artifact.id, artifact.udf[result_udf],artifact.udf[udf1],operator,artifact.udf[udf2])
+            artifact.id, artifact.udf[result_udf],artifact.udf[udf1],op,artifact.udf[udf2])
         artifact.udf[result_udf] = eval('{0}{1}{2}'.format(artifact.udf[udf1],op,artifact.udf[udf2]))
         artifact.put()
         print 'Updated {0} to {1}.'.format(result_udf,artifact.udf[result_udf])
