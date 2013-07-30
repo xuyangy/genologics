939555e5465c69c7b36a4b28cc6cc24dab6ff4e1
Johannes Alneberg <johannes.alneberg@scilifelab.se>
2013-07-30 17:01:54 +0200

    Replaced accidentaly deleted stuff.

--- genologics/entities.py
+++ genologics/entities.py
@@ -485,7 +485,8 @@ class Entity(object):
     @property
     def id(self):
         "Return the LIMS id; obtained from the URI."
-        
+        parts = urlparse.urlsplit(self.uri)
+        return parts.path.split('/')[-1]
 
     def get(self, force=False):
         "Get the XML data for this instance."
