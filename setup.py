b0e80ecdeb5666d68e8d65b76b4f570c2ca78568
Johannes Alneberg <johannes.alneberg@scilifelab.se>
2013-08-26 14:26:13 +0200

    Python 2.6 friendly version checking

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import subprocess
 version_py = os.path.join(os.path.dirname(__file__), 'version.py')
 
 try:
-    version_git = subprocess.check_output(["git", "describe"]).rstrip()
+    version_git = subprocess.Popen(["git", "describe"],stdout=subprocess.PIPE).communicate()[0].rstrip()
 except:
     with open(version_py, 'r') as fh:
         version_git = open(version_py).read().strip().split('=')[-1].replace('"','')
