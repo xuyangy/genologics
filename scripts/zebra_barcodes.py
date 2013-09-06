6ca25a301c560ee3a5a13c7a59ccc78f27fa6c20
Johannes Alneberg <johannes.alneberg@scilifelab.se>
2013-09-06 16:27:14 +0200

    Removed old code from zebra_barcode

--- scripts/zebra_barcodes.py
+++ scripts/zebra_barcodes.py
@@ -9,31 +9,6 @@ from genologics.lims import Lims
 from genologics.epp import EppLogger
 from genologics.entities import Process
 
-def construct(*args, **kwargs):
-    start = int(kwargs.get('start'))
-    end = int(kwargs.get('end'))
-    
-    for i in range(start, end):
-        PID = "P"+str(i)
-        makeProjectBarcode(PID)
-    
-def makeProjectBarcode(PID):
-    print "^XA" #start of label
-    print "^DFFORMAT^FS" #download and store format, name of format, end of field data (FS = field stop)
-    print "^LH0,0" # label home position (label home = LH)
-    print "^FO360,20^AFN,60,20^FN1^FS" #AF = assign font F, field number 1 (FN1), print text at position field origin (FO) rel. to home
-    print "^FO140,5^BCN,70,N,N^FN2^FS" #BC=barcode 128, field number 2, Normal orientation, height 70, no interpreation line. 
-    print "^XZ" #end format
-    
-    for i in range (1,6):
-        PlateID="P"+str(i)
-        plateCode=PID+PlateID
-        print "^XA" #start of label format
-        print "^XFFORMAT^FS" #label home posision
-        print "^FN1^FD"+plateCode+"^FS" #this is readable
-        print "^FN2^FD"+plateCode+"^FS" #this is the barcode
-        print "^XZ"
-
 def makeContainerBarcode(plateid,copies=1):
     lines = []
     lines.append("^XA") #start of label
