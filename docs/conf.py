99135d09acfe2bd678dcc976957f17f70b914b66
Johannes Alneberg <johannes.alneberg@scilifelab.se>
2013-10-04 13:58:29 +0200

    Removed Mock class, did not work as expected

--- docs/conf.py
+++ docs/conf.py
@@ -23,33 +23,6 @@ import sys, os
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
 
-# Mock class for avoiding errors when importing genologics
-import sys
-
-class Mock(object):
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def __call__(self, *args, **kwargs):
-        return Mock()
-
-    @classmethod
-    def __getattr__(cls, name):
-        if name in ('__file__', '__path__'):
-            return '/dev/null'
-        elif name[0] == name[0].upper():
-            mockType = type(name, (), {})
-            mockType.__module__ = __name__
-            return mockType
-        else:
-            return Mock()
-
-MOCK_MODULES = ['genologics','genologics.config','genologics.lims',
-                'genologics.entities','genologics.epp']
-
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = Mock()
-
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 
