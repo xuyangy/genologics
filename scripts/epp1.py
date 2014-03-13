8254beffc2228758b1c3a95b13ad6b888364511b
maya.brandi <maya.brandi@genologics-stage.scilifelab.se>
2014-03-13 10:17:21 +0100

    quantit class added. first trial

--- scripts/epp1.py
+++ scripts/epp1.py
@@ -25,90 +25,111 @@ from genologics.epp import EppLogger
 from genologics.epp import ReadResultFiles
 lims = Lims(BASEURI,USERNAME,PASSWORD)
 
+class QunatiT():
+    def __init__(self, process):
+        self.file_handler = ReadResultFiles(process)
+        self.udfs = dict(process.udf.items())
+        self.abstract = []
+        self.missing_udfs = []
+        self.standards = self._make_standards_list()
+        self.mod, self.R2 = self._verify_standards()
 
-def make_standards_list(result_file):
-    standards_dict = {}
-    standards_list = np.ones(8)
-    for k,v in result_file.items():
-        if set(['Sample','End RFU']).issubset(v) and v['Sample'].split()[0]=='Standard':
-            standard = int(v['Sample'].split()[1])
-            standards_dict[standard] = float(v['End RFU'])
-    for k,v in standards_dict.items():
-        standards_list[k-1] = v - standards_dict[1]
-    return standards_list
+    def _formated_result_files_dict(self):
+        result_files = {}
+        for f_name in ['Quant-iT Result File 1','Quant-iT Result File 2']:
+            if self.file_handler.shared_files.has_key(f_name):
+                result_file = file_handler.shared_files[f_name]
+                result_files[f_name] = self.file_handler.format_file(result_file,
+                                                        first_header = 'Sample', root_key_col = 1)
+        return result_files
+ 
+    def _make_standards_list(self):
+        standards_file = self.file_handler.shared_files['Standards File (.txt)']
+        standards_file_formated, warn = self.file_handler.format_file(standards, header_row = 26)
+        standards_dict = {}
+        for k,v in standards_file_formated.items():
+            if set(['Sample','End RFU']).issubset(v) and v['Sample'].split()[0]=='Standard':
+                standard = int(v['Sample'].split()[1])
+                standards_dict[standard] = float(v['End RFU'])
+        return standards_dict
 
-def amount_in_standards(standard_volume, standard_dilution, assay_type):
-    nuclear_acid_amount = np.ones(8)
-    supp_conc_stds = {'RNA BR':[0,5,10,20,40,60,80,100],
-                      'RNA':[0,5,10,20,40,60,80,100]}    
-    for standard in range(8):
-        nuclear_acid_amount[standard] = np.true_divide(supp_conc_stds[assay_type][standard]*standard_volume, standard_dilution)
-    return nuclear_acid_amount
+    def _amount_in_standards(self):
+        nuclear_acid_amount = np.ones(8)
+        supp_conc_stds = {'RNA BR':[0,5,10,20,40,60,80,100],
+                          'RNA':[0,5,10,20,40,60,80,100]}    
+        if set(['Standard volume','Assay type','Standard dilution']).issubset(self.udfs.keys())
+            for standard in range(8):
+                nuclear_acid_amount[standard] = np.true_divide(supp_conc_stds[self.udf['Assay type']][standard] * 
+                    self.udfs['Standard volume'], self.udfs['Standard dilution'])
+            return nuclear_acid_amount
+        else:
+            self.missing_udfs += ['Standard volume','Assay type','Standard dilution']
+            return None
 
-def linear_regression(X,Y):
-    "Returns slope and intersect of linear regression on lists X and Y"
-    A = np.array([ X, np.ones(len(X))])
-    mod, resid = np.linalg.lstsq(A.T,Y)[:2]
-    R2 = 1 - resid / (Y.size * Y.var())
-    return R2, mod
+    def _linear_regression(self, X,Y):
+        "Returns slope and intersect of linear regression on lists X and Y"
+        A = np.array([ X, np.ones(len(X))])
+        mod, resid = np.linalg.lstsq(A.T,Y)[:2]
+        R2 = 1 - resid / (Y.size * Y.var())
+        return R2, mod
 
-def verify_standards(standards_file, udfs):
-    X = make_standards_list(standards_file)
-    Y = amount_in_standards(udfs['standard_volume'], 
-                    udfs['standard_dilution'], udfs['assay_type'])
-    R2, mod = linear_regression(X,Y)
-    if R2 >= udfs['linearity_of_standards']:
-        return mod, "R2 = {0}. Standards OK. Upload input file(s) for samples".format(R2) 
-    else: 
-        return mod, "R2 = {0}. Problem with standards! Redo measurement!".format(R2)
+    def _verify_standards(self):
+        relative_standards = np.ones(8)
+        for k,v in self.standards.items(): 
+            relative_standards[k-1] = v - self.standards[1]
+        amount_in_standards = self._amount_in_standards()
+        R2, mod = self._linear_regression(relative_standards, amount_in_standards)
+        if 'Linearity of standardse' in self.udfs.keys():
+            if R2 >= self.udfs['Linearity of standards']:
+                self.abstract.append("R2 = {0}. Standards OK. Upload input file(s) for samples".format(R2))
+            else:  
+                self.abstract.append("R2 = {0}. Problem with standards! Redo measurement!".format(R2))
+            return mod, R2
+        else:
+            self.abstract.append("Kould not verify standards. Please set 'Linearity of standards'.")
+            self.missing_udfs.append('Linearity of standards')
+        return mod, R2
 
-def calculate_concentration(mod, target_file, sample_volume, result_files):
-    sample = target_file.samples[0].name
-    fluor_int = []
-    for f_name ,formated_file in result_files.items():
-        if sample in formated_file:
-            fluor_int.append(formated_file[sample]['End RFU'])
-            target_file.udf[f_name] = formated_file[sample]['End RFU']  
-    mean_fluor_int = np.mean(fluor_int)
-    rel_fluor_int = mean_fluor_int - End_RFU_of_Standard_1
-    conc = np.true_divide((slope * rel_fluor_int + intersect) , sample_volume)
-    target_file.udf['Concentration'] = conc
-    target_file.udf['Conc. Units'] = 'ng/ul'
-    return target_file
+    def calculate_concentration(self, target_file):
+        result_files = self._formated_result_files_dict()
+        sample = target_file.samples[0].name
+        slope = self.mod[0]
+        intersect = self.mod[1]
+        fluor_int = []
+        for f_name ,formated_file in result_files.items():
+            if sample in formated_file:
+                fluor_int.append(formated_file[sample]['End RFU'])
+                target_file.udf[f_name] = formated_file[sample]['End RFU']  
+        mean_fluor_int = np.mean(fluor_int)
+        rel_fluor_int = mean_fluor_int - self.standards[1]
+        if 'Sample volume' in self.udfs.keys():
+            conc = np.true_divide((slope * rel_fluor_int + intersect) , self.udfs['Sample volume'])
+            target_file.udf['Concentration'] = conc
+            target_file.udf['Conc. Units'] = 'ng/ul'
+        else:
+            self.missing_udfs.append('Linearity of standards')
+        return target_file
+
 
 def main(lims, pid, epp_logger):
     process = Process(lims,id = pid)
-    udfs = {}
-    result_files = {}
-    file_handler = ReadResultFiles(process)
-    standards_file = file_handler.shared_files['Standards File (.txt)']
-    standards_file_formated, warn = file_handler.format_file(standards_file,header_row = 26)
-    for f_name in ['Quant-iT Result File 1','Quant-iT Result File 2']:
-        if file_handler.shared_files.has_key(f_name):
-            quantit_result_file = file_handler.shared_files[f_name]
-            result_files[f_name] = file_handler.format_file(quantit_result_file, first_header = 'Sample', root_key_col = 1)
+    qunatit = QunatiT(process)
     target_files = process.result_files()
-    result_files = {}
-    
-    try:
-        udfs['assay_type'] = process.udf['Assay type']
-        udfs['standard_volume'] = process.udf['Standard volume']
-        udfs['linearity_of_standards'] = process.udf['Linearity of standards']
-        udfs['standard_dilution'] = process.udf['Standard dilution']    
-        udfs['sample_volume'] = process.udf['Sample volume']
-    except:
-        abstract = """process udfs missing. Please make sure 'Assay type', 
-                    'Standard volume', 'Linearity of standards', 'Sample volume'
-                     and 'Standard dilution' are well defined."""
-    if udfs:
-        mod, abstract = verify_standards(standards_file_formated, udfs)
+
+    if 'Sample volume' in qunatit.udfs.keys(): #and result files....
         for target_file in target_files:
-            target_file = calculate_concentration(mod, target_file, udfs['sample_volume'], result_files)
+            target_file = qunatit.calculate_concentration(mod, target_file)
             try:
                 target_file.put()
             except (TypeError, HTTPError) as e:
                 logging.warning("Error while updating element: {0}".format(e))
-
+    else:
+        qunatit.abstract.append("Kould not calculate concentration. Please set 'Linearity of standards'.")
+    
+    if qunatit.missing_udfs:
+        qunatit.abstract.append("Are all of the folowing udfs set? : {0}".format(', '.join(qunatit.missing_udfs)))
+    
+    abrtract = ' '.join(qunatit.abstract)
 
     logging.info(abstract)
     print >> sys.stderr, abstract
