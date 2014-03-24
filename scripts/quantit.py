58f62204f1efb75154b3af78a61c130d814c7eeb
maya.brandi <maya.brandi@genologics-stage.scilifelab.se>
2014-03-24 20:33:44 +0100

    first tryal

--- scripts/quantit.py
+++ scripts/quantit.py
@@ -128,8 +128,12 @@ class QunatiT():
         Y = slope*X + intersect; R2=Pearson correlation coefficient."""
         relative_standards = np.ones(8)
         for k,v in self.standards.items(): 
+            print k
+            print v
             relative_standards[k-1] = v - self.standards[1]
         amount_in_standards = self._nuclear_acid_amount_in_standards()
+        print amount_in_standards
+        print relative_standards
         R2, slope, intersect = self._linear_regression(relative_standards, amount_in_standards)
         return R2, slope, intersect
 
