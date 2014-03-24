89942df9197a3c7477ff8bbcde2f738041558b79
maya.brandi <maya.brandi@genologics-stage.scilifelab.se>
2014-03-25 00:08:43 +0100

    EPP2

---
+++ scripts/quantit_generate_driver_file.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+DESC = """EPP script for Quant-iT mesurements to generate a driver file (.csv) 
+which include the Sample names and their positions in the working plate.
+
+Reads from:
+    --Lims fields--
+    "location"      field of input analytes to the process
+
+Writes to:
+    --file--
+    "Driver File"   shared result file
+
+Logging:
+The script outputs a regular log file with regular execution information.
+
+Written by Maya Brandi 
+"""
+
+import os
+import sys
+import logging
+import numpy as np
+
+from argparse import ArgumentParser
+from requests import HTTPError
+from genologics.lims import Lims
+from genologics.config import BASEURI,USERNAME,PASSWORD
+from genologics.entities import Process
+from genologics.epp import EppLogger
+from genologics.epp import set_field
+from genologics.epp import ReadResultFiles
+lims = Lims(BASEURI,USERNAME,PASSWORD)
+
+class QunatiT():
+    def __init__(self, process):
+        self.udfs = dict(process.udf.items())
+        self.abstract = []
+        self.no_samples = 0
+
+    def make_location_dict(self, input_analytes):
+        location_dict = {}
+        for sample, input_analyte in input_analytes.items():
+            try:
+                well = input_analyte.location[1]
+                row, col = well.split(':')
+                location_dict[well] = ','.join([row, col,'', sample])
+            except:
+                self.no_samples +=1
+        return location_dict
+
+    def make_file(self, location_dict):
+        keylist = location_dict.keys()
+        keylist.sort()
+        f = open('QuantiT_driver_file_exported_from_LIMS.csv','a')
+        print >> f , 'Row,Column,*Target Name,*Sample Name'
+        for key in keylist:
+            print >> f ,location_dict[key]
+        f.close()
+
+
+def main(lims, pid, epp_logger):
+    process = Process(lims,id = pid)
+    QiT = QunatiT(process)
+    input_analytes = dict((a.name, a) for a in process.analytes()[0])
+    location_dict = QiT.make_location_dict(input_analytes)
+    if QiT.no_samples:
+        QiT.abstract.append("Could not get location for {0} samples.".format(QiT.no_samples))
+    QiT.make_file(location_dict)
+
+    print >> sys.stderr, ' '.join(QiT.abstract)
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description=DESC)
+    parser.add_argument('--pid', default = None , dest = 'pid',
+                        help='Lims id for current Process')
+    parser.add_argument('--log', dest = 'log',
+                        help=('File name for standard log file, '
+                              'for runtime information and problems.'))
+
+    args = parser.parse_args()
+    lims = Lims(BASEURI,USERNAME,PASSWORD)
+    lims.check_version()
+
+    with EppLogger(log_file=args.log, lims=lims, prepend=True) as epp_logger:
+        main(lims, args.pid, epp_logger)
+
