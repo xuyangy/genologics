896a93c80da52e677ce8ae70ef430769c54f5daa
maya.brandi <maya.brandi@genologics-stage.scilifelab.se>
2014-03-31 14:46:19 +0200

    rm old flourecence intencity from udf if not given as input

--- scripts/quantit_set_conc.py
+++ scripts/quantit_set_conc.py
@@ -152,10 +152,10 @@ class QunatiT():
         rel_fluor_int = The End RFU of standards - Background fluorescence intensity"""
         sample = target_file.samples[0].name
         fluor_int = []
-        target_udfs = target_file.udf.items()
-        if dict(target_udfs).has_key("Fluorescence intentisy 1"): 
+        target_udfs = target_file.udf
+        if dict(target_udfs.items()).has_key("Fluorescence intentisy 1"): 
             del target_udfs["Fluorescence intentisy 1"]
-        if dict(target_udfs).has_key("Fluorescence intentisy 2"): 
+        if dict(target_udfs.items()).has_key("Fluorescence intentisy 2"): 
             del target_udfs["Fluorescence intentisy 2"]
         target_file.udf = target_udfs
         for udf_name ,formated_file in self.result_files.items():
