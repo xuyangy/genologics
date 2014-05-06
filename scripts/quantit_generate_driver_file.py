f60185b5a75046a76f14742cc16a8b9c574b77c3
maya.brandi <maya.brandi@genologics-stage.scilifelab.se>
2014-05-06 08:59:37 +0200

    test

--- scripts/quantit_generate_driver_file.py
+++ scripts/quantit_generate_driver_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-DESC = """EPP script for Quant-iT mesurements to generate a driver file (.csv) 
+DESC = """EPP script for Quant-iT measurements to generate a driver file (.csv) 
 which include the Sample names and their positions in the working plate.
 
 Reads from:
@@ -35,6 +35,9 @@ class QuantitDriverFile():
         self.drivf = drivf
 
     def make_location_dict(self, io_filtered):
+        """Loops through the input-output map and formates the 
+        well location info into the driver file formate: 
+        row,col,,sample_name"""
         location_dict = {}
         for input, output in io_filtered:
             well = output['uri'].location[1]
@@ -44,13 +47,14 @@ class QuantitDriverFile():
         return location_dict
 
     def make_file(self, location_dict):
+        """Writes the formated well location info into a driver 
+        file sorted by row and col."""
         keylist = location_dict.keys()
         keylist.sort()
-        f = open( self.drivf, 'a')
+        open(self.drivf, 'a') as f
         print >> f , 'Row,Column,*Target Name,*Sample Name'
         for key in keylist:
             print >> f ,location_dict[key]
-        f.close()
 
 def main(lims, pid, drivf ,epp_logger):
     process = Process(lims,id = pid)
@@ -68,8 +72,9 @@ if __name__ == "__main__":
     parser.add_argument('--log', dest = 'log',
                         help=('File name for standard log file, '
                               'for runtime information and problems.'))
-    parser.add_argument('--drivf', dest = 'drivf', default = 'QuantiT_driver_file_exported_from_LIMS.csv',
-                            help=('File name for Driver file to be generated'))
+    parser.add_argument('--drivf', dest = 'drivf', 
+                        default = 'QuantiT_driver_file_exported_from_LIMS.csv',
+                        help=('File name for Driver file to be generated'))
 
     args = parser.parse_args()
     lims = Lims(BASEURI,USERNAME,PASSWORD)
