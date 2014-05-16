e2f1a809912f3a21971aeba255e0172614fc5774
maya.brandi <maya.brandi@genologics-stage.scilifelab.se>
2014-05-16 14:57:16 +0200

    debugging missing pool names

--- scripts/copy_qubit.py
+++ scripts/copy_qubit.py
@@ -52,7 +52,8 @@ def main(lims, pid, epp_logger):
         min_conc = None
         abstract.append("Set 'Minimum required concentration (ng/ul)' to get qc-flaggs based on this treshold!")
     for target_file in target_files:
-        print target_file.name
+        print target_file.udf.items()
+        print target_file.__dict__()
         print dir(target_file)
         sample = target_file.samples[0].name
         if qubit_result_file.has_key(sample):
