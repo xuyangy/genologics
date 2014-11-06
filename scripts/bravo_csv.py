f5da4bad954d01970d5963f67eee62f56f03162e
Denis Moreno <denismoreno@denismmbp.scilifelab.se>
2014-11-06 15:03:25 +0100

    cleaned / reorganized

--- scripts/bravo_csv.py
+++ scripts/bravo_csv.py
@@ -6,27 +6,31 @@ from genologics.lims import Lims
 from genologics.config import BASEURI,USERNAME,PASSWORD
 from genologics.epp import attach_file, EppLogger
 import logging
-import re
+import os
 from genologics.entities import *
 
 def main(lims, args):
     currentStep=Process(lims,id=args.pid)
-    for art_tuple in currentStep.input_output_maps:
-        if art_tuple[0]['uri'].type=='Analyte' and art_tuple[1]['uri'].type=='Analyte': 
-            source_fc=art_tuple[0]['uri'].location[0].name
-            source_well=art_tuple[0]['uri'].location[1]
-            dest_fc=art_tuple[1]['uri'].location[0].name
-            dest_well=art_tuple[1]['uri'].location[1]
-            volume=calc_vol(art_tuple)
-            print art_tuple[0]['uri'].samples[0].name
-            print "{0},{1},{2},{3},{4}".format(source_fc, source_well, volume, dest_fc, dest_well)
-
+    with open("bravo.csv", "w") as stupidContext:
+        for art_tuple in currentStep.input_output_maps:
+            if art_tuple[0]['uri'].type=='Analyte' and art_tuple[1]['uri'].type=='Analyte': 
+                source_fc=art_tuple[0]['uri'].location[0].name
+                source_well=art_tuple[0]['uri'].location[1]
+                dest_fc=art_tuple[1]['uri'].location[0].name
+                dest_well=art_tuple[1]['uri'].location[1]
+                volume=calc_vol(art_tuple)
+                stupidContext.write("{0},{1},{2},{3},{4}\n".format(source_fc, source_well, volume, dest_fc, dest_well)) 
+    for out in currentStep.all_outputs():
+        if out.name=="Bravo CSV File":
+            attach_file(os.path.join(os.getcwd(), "bravo.csv"), out)
+    logging.info("Work done")
 def calc_vol(art_tuple):
     try:
         assert art_tuple[0]['uri'].udf['Conc. Units'] == "ng/ul"
         amount_ng=art_tuple[1]['uri'].udf['Amount taken (ng)']
         conc=art_tuple[0]['uri'].udf['Concentration']
-        return amount_ng/conc
+        print amount_ng, conc, "{0:.2f}".format(amount_ng/conc)
+        return "{0:.2f}".format(amount_ng/conc) 
     except KeyError as e:
         logging.error("The input artifact is lacking a field : {}".format(e)) 
     except AssertionError:
