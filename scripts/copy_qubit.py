29ddfb22e2a11b003fe3b7f14bc62a6a24a08fbb
Denis Moreno <denismoreno@DenisMMBP.scilifelab.se>
2015-03-26 14:38:14 +0100

    qubit updated

--- scripts/copy_qubit.py
+++ scripts/copy_qubit.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
 
 DESC = """EPP script to copy 'concentration' and 'concentration unit' for each 
 sample sample in the 'Qubit Result File' to the 'Concentration' and 'Conc. Units' 
@@ -14,13 +16,14 @@ contains regular execution information in the folowing cases:
  
 Can be executed in the background or triggered by a user pressing a "blue button".
 
-Written by Maya Brandi 
+Written by Maya Brandi and Denis Moreno
 """
 
 import os
 import sys
 import logging
 import numpy as np
+import codecs
 
 from argparse import ArgumentParser
 from requests import HTTPError
@@ -31,7 +34,129 @@ from genologics.epp import EppLogger
 from genologics.epp import ReadResultFiles
 from genologics.epp import set_field
 
+import csv
+
+def get_qbit_file(process):
+        outs = process.all_outputs()
+        for outart in outs:
+            #get the right output artifact
+            if outart.type == 'ResultFile' and outart.name == 'Qubit Result File':
+                #get the associated file
+                csvfile = outart.files[0]
+                #content location is a ugly sftp uri
+                file_path = csvfile.content_location.split('scilifelab.se')[1]
+                return file_path
+        return None
+
+def get_data(file_path):
+    read=False
+    data={}
+    utflines=[]
+    with codecs.open(file_path ,'rb', 'iso-8859-1') as f:
+        #utf-8 translation, thanks qubit. 
+        for l in f:
+            utflines.append(l.encode('utf-8'))
+
+        pf = csv.reader(utflines)
+        #New Qubit
+        #defaults
+        sample_index=2
+        conc_index=6
+        conc_unit=7
+        for row in pf:
+            if 'Test Name' in row:
+                #this is the header row
+                sample_index=row.index('Test Name')
+                conc_index=row.index('Original sample conc.')
+                unit_index=conc_index+1
+                read=True
+            elif(read and row[sample_index]):
+                #this is every other row
+                data[row[sample_index]]={}
+                data[row[sample_index]]['concentration']=row[conc_index]
+                data[row[sample_index]]['unit']=row[unit_index]
+            elif (not read  and 'Sample' in row):
+                sample_index=row.index('Sample')
+                conc_index=row.index('Sample Concentration')
+                unit_index=conc_index+1
+                read=True
+    return data
+
+def convert_to_ng_ul(conc, unit):
+    factor=1
+    units=split('/',unit)
+    if units[0] == 'µg' or units[0] == 'ug':
+        factor*=1000
+    elif units[0] == 'mg':
+        factor*=1000000
+    if units[1] == 'mL':
+        factor/=1000
+    if units[1] == 'nL':
+        factor*=1000
+
+    return conc*factor
+
+
+def get_qbit_csv_data(process):
+    #samples missing from the qubit csv file
+    missing_samples = 0
+    low_conc = 0
+    bad_format = 0
+    #strings returned to the EPP user
+    log = []
+
+    #get file path by parsing lims artifacts
+    file_path=get_qbit_file(process)
+    #parse the qubit file and get the interesting data out
+    data=get_data(file_path)
+
+    if "Minimum required concentration (ng/ul)" in process.udf:
+        min_conc=process.udf['Minimum required concentration (ng/ul)']
+    else:
+        min_conc=None
+        log.append("Set 'Minimum required concentration (ng/ul)' to get qc-flags based on this threshold!")
+
+    for target_file in process.result_files():
+        file_sample=target_file.samples[0].name
+        if file_sample in data:
+            try:
+                conc=float(data[file_sample]['concentration'])
+            except ValueError:
+                #concentration is not a float
+                target_file.qc_flag = "FAILED"
+                if data[file_sample]['concentration'] != 'Out of range':
+                    #Out of range is a valid value, the others are not.
+                    bad_format+=1
+
+            else:
+                new_conc=convert_to_ng_ul(conc, data[file_sample]['unit'])
+                if min_conc and new_conc < min_conc:
+                    target_file.qc_flag = "FAILED"
+                    low_conc +=1
+                else:
+                    target_file.qc_flag = "PASSED"
+                    target_file.udf['Concentration'] = conc
+                    target_file.udf['Conc. Units'] = 'ng/ul'
+
+            #actually set the data
+            set_field(target_file)
+        else:
+            missing_samples += 1
+    if low_conc:
+        log.append('{0}/{1} samples have low concentration.'.format(low_conc, len(target_files)))
+    if missing_samples:
+        log.append('{0}/{1} samples are missing in the Qubit Result File.'.format(missing_samples, len(target_files)))
+    if bad_format:
+        log.append('There are {0} badly formatted samples in Qubit Result File. Please fix these to get proper results.'.format(bad_format))
+
+    print(''.join(log), file=sys.stderr)
+
 def main(lims, pid, epp_logger):
+
+    process = Process(lims,id = pid)
+    get_qbit_csv_data(process)
+
+def old_main(lims, pid, epp_logger):
     process = Process(lims,id = pid)
     sample_names = map(lambda a: a.name, process.analytes()[0])
     target_files = process.result_files()
