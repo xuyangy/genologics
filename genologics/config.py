3b70502f3fd3630eec9816c58bbf021104d9dca2
juliambrosman <julia.margaret.brown@gmail.com>
2016-03-03 16:20:42 -0500

    tab to space in config.py

--- genologics/config.py
+++ genologics/config.py
@@ -4,6 +4,7 @@ import warnings
 
 import ConfigParser
 
+#specified_config = "/Users/julia/Desktop/LIMS/config/.genologicsprod"
 specified_config = None
 
 def get_config_info(config_file):
@@ -12,34 +13,33 @@ def get_config_info(config_file):
     
     
     BASEURI = config.get('genologics', 'BASEURI').rstrip()
-	USERNAME = config.get('genologics', 'USERNAME').rstrip()
-	PASSWORD = config.get('genologics', 'PASSWORD').rstrip()
-	
-	if config.has_section('genologics') and config.has_option('genologics','VERSION'):
-		VERSION = config.get('genologics', 'VERSION').rstrip()
-	else:
-		VERSION = 'v2'
-		
-	if config.has_section('logging') and config.has_option('logging','MAIN_LOG'):
-		MAIN_LOG = config.get('logging', 'MAIN_LOG').rstrip()
-	else:
-		MAIN_LOG = None
-	return BASEURI, USERNAME, PASSWORD, VERSION, MAIN_LOG
-		
-
+    USERNAME = config.get('genologics', 'USERNAME').rstrip()
+    PASSWORD = config.get('genologics', 'PASSWORD').rstrip()
+    
+    if config.has_section('genologics') and config.has_option('genologics','VERSION'):
+        VERSION = config.get('genologics', 'VERSION').rstrip()
+    else:
+        VERSION = 'v2'
+        
+    if config.has_section('logging') and config.has_option('logging','MAIN_LOG'):
+        MAIN_LOG = config.get('logging', 'MAIN_LOG').rstrip()
+    else:
+        MAIN_LOG = None
+    return BASEURI, USERNAME, PASSWORD, VERSION, MAIN_LOG
+        
 if specified_config:
-	config_file = specified_config
+    config_file = specified_config
 else:
-	config = ConfigParser.SafeConfigParser()
-	try:
-		conf_file = config.read([os.path.expanduser('~/.genologicsrc'), '.genologicsrc',
-					'genologics.conf', 'genologics.cfg', '/etc/genologics.conf'])
+    config = ConfigParser.SafeConfigParser()
+    try:
+        conf_file = config.read([os.path.expanduser('~/.genologicsrc'), '.genologicsrc',
+                    'genologics.conf', 'genologics.cfg', '/etc/genologics.conf'])
 
-		# First config file found wins
-		config_file = conf_file[0]
-	
-	except:
-		warnings.warn("Please make sure you've created your own Genologics configuration file (i.e: ~/.genologicsrc) as stated in README.md")
-		sys.exit(-1)
+        # First config file found wins
+        config_file = conf_file[0]
+    
+    except:
+        warnings.warn("Please make sure you've created your own Genologics configuration file (i.e: ~/.genologicsrc) as stated in README.md")
+        sys.exit(-1)
 
 BASEURI, USERNAME, PASSWORD, VERSION, MAIN_LOG = get_config_info(config_file)
