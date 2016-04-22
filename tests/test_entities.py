7d6c850c951d4685843e8a6fee9aeaef8c04c0ce
Denis Moreno <denis.moreno@scilifelab.se>
2016-04-22 15:05:29 +0200

    no need to compare whitespace

--- tests/test_entities.py
+++ tests/test_entities.py
@@ -228,8 +228,8 @@ class TestUdfDictionary(TestCase):
 
 def elements_equal(e1, e2):
     if e1.tag != e2.tag: return False
-    if e1.text != e2.text: return False
-    if e1.tail != e2.tail: return False
+    if e1.text and e2.text and e1.text.strip() != e2.text.strip(): return False
+    if e1.tail and e2.tail and e1.tail.strip() != e2.tail.strip(): return False
     if e1.attrib != e2.attrib: return False
     if len(e1) != len(e2): return False
     return all(elements_equal(c1, c2) for c1, c2 in zip(e1, e2))
