ecef470bde7298b32bf7c7097ebfde36dffe13fe
tcezard <timothee.cezard@ed.ac.uk>
2017-02-21 15:27:59 +0000

    Make sure the UDFDictionary is emptied before setting a new one

--- genologics/descriptors.py
+++ genologics/descriptors.py
@@ -349,6 +349,7 @@ class UdfDictionaryDescriptor(BaseDescriptor):
     def __set__(self, instance, dict_value):
         instance.get()
         udf_dict = UdfDictionary(instance, *self.rootkeys, udt=self._UDT)
+        udf_dict.clear()
         for k in dict_value:
             udf_dict[k] = dict_value[k]
 
